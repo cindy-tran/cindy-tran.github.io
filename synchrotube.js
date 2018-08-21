47b82c5e0a8dcf80b2fb29f541ace4e9e818c91a
cindy-tran <cindyntran96@gmail.com>
2018-08-20 19:41:15 -0700

    made a few changes

---
+++ synchrotube.js
@@ -0,0 +1,11 @@
+// TODOS:
+// - figure out how to use socket.io with Youtube API
+// - users can log in + log out
+// - users can create a room 
+// - users can chat with other users in the room
+// - the moderator can set a video to watch 
+// - any one can pause, play, or change the time in the video
+
+
+
+
